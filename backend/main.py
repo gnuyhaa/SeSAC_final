d750ba437ae8fa266dbb6b9d41be050534b7b2c0
gnuyhaa <guddk18@naver.com>
2025-10-01 19:53:42 +0900

    수정:코드수정

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.include_router(emotions.router) # emotions 라우터
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
