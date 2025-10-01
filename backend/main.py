4e9b2c80d30f066cf141165ee517fec8582e2abd
gnuyhaa <guddk18@naver.com>
2025-10-01 20:32:12 +0900

    수정:코드수정

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.include_router(emotions.router) # emotions 라우터
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["https://growth-doctor-front.web.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
