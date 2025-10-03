c7bee0eb4f092566f92f5f4e089edea374ec0e5a
gnuyhaa <guddk18@naver.com>
2025-10-03 15:03:04 +0900

    수정:cors 설정 수정

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import auth, parks, park_category, emotions, recommend
+from .routers import auth, parks, park_category, emotions, recommend_parks, recommend_category, recommend_for_user
 from fastapi.middleware.cors import CORSMiddleware
 import logging
 
@@ -13,12 +13,14 @@ app.include_router(auth.router)   # auth 라우터
 app.include_router(parks.router)  # parks 라우터
 app.include_router(park_category.router) # park_category 라우터
 app.include_router(emotions.router) # emotions 라우터
-app.include_router(recommend.router) # recommend 라우터
+app.include_router(recommend_parks.router) # recommend_parks 라우터
+app.include_router(recommend_category.router) # recommend_parks 라우터
+app.include_router(recommend_for_user.router) # recommend_parks 라우터
 
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://growth-doctor-front.web.app", "http://localhost:3000"],  
+    allow_origins=["https://growth-doctor-front.web.app", "http://localhost:3000", "http://172.16.20.116:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
