37d300aebd4376daec030478964ada0d09b4c167
gnuyhaa <guddk18@naver.com>
2025-10-10 15:39:58 +0900

    수정: llm 라우터 추가

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from routers import auth, parks, emotions, recommend_parks, recommend_category, recommend_for_user, visit
+from routers import auth, parks, emotions, visit
+from routers import recommend_parks, recommend_category, recommend_for_user
+from routers import generate_summary, generate_weekly_review
 from fastapi.middleware.cors import CORSMiddleware
 import logging
 
@@ -16,7 +18,8 @@ app.include_router(recommend_parks.router) # recommend_parks 라우터
 app.include_router(recommend_category.router) # recommend_parks 라우터
 app.include_router(recommend_for_user.router) # recommend_parks 라우터
 app.include_router(visit.router) # visit 라우터
-
+app.include_router(generate_summary.router) 
+app.include_router(generate_weekly_review.router)
 
 # CORS 설정
 app.add_middleware(
