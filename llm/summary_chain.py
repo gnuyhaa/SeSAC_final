3c3a1dbe1ac381a23864f304b2a54fcace03ba6a
yusun <1223yusun@gmail.com>
2025-10-10 18:57:10 +0900

    수정: 데이터 가져오기 및 요약 내용 수정

--- llm/summary_chain.py
+++ llm/summary_chain.py
@@ -25,10 +25,13 @@ def summary(nickname: str):
     # 날짜로 내림차순을 해서 첫번째만 가져오기 = 제일 최신 사용 정보 가져오기
     cur.execute("""
         SELECT e.nickname, e.create_date, depression, anxiety, stress, happiness, achievement, 
-            energy, category_1, category_2, category_3, park_1, park_2, park_3
-        FROM tb_users_emotions e 
-        JOIN tb_users_category_recommend u ON u.create_date  = e.create_date 
-        JOIN tb_users_parks_recommend p ON e.create_date  = p.create_date 
+            energy, category_1, category_2, category_3,
+            p.park_1 AS park_1,
+            p.park_2 AS park_2,
+            p.park_3 AS park_3
+        FROM tb_users_emotions e
+        LEFT JOIN tb_users_category_recommend u ON u.create_date = e.create_date AND u.nickname = e.nickname
+        LEFT JOIN tb_users_parks_recommend p ON p.create_date = e.create_date AND p.nickname = e.nickname
         WHERE e.nickname = %s
         ORDER BY e.create_date DESC
         LIMIT 1;
@@ -44,12 +47,6 @@ def summary(nickname: str):
     행복: {happiness}
     성취감: {achievement}
     에너지: {energy}
-    category_1: {category_1}
-    category_2: {category_2}
-    category_3: {category_3}
-    park_1: {park_1}
-    park_2: {park_2}
-    park_3: {park_3}
 
     Instructions:
     1. 감정 top3를 점수 순으로 내림차순 정렬해서 "top_emotions"에 담아주세요.
@@ -57,16 +54,12 @@ def summary(nickname: str):
     2. 각 감정은 {{"감정명": 점수}} 형태로 작성
     - 예: [{{"우울": 3}}, {{"불안": 2}}, {{"행복": 5}}]
     3. 전체 감정의 흐름을 자연어로 요약해서 "emotions_summary"에 한 줄(50자 이내)로 담아주세요.
-    4. 추천 카테고리(category_1~3)를 **각각 짧게 요약**해서 리스트 형태로 "recommand_cates"에 담아주세요.
-    5. 추천 공원(park_1~3)을 그대로(**절대 이름을 바꾸지 말고**) 리스트 형태로 "recommand_parks"에 담아주세요.
-    6. JSON 형식을 정확히 지켜주세요.
+    4. JSON 형식을 정확히 지켜주세요.
 
     Return JSON in this exact format:
     {{
     "top_emotions": [],
-    "emotions_summary": "",
-    "recommand_cates": [],
-    "recommand_parks": []
+    "emotions_summary": ""
     }}
     """)
 
@@ -74,6 +67,10 @@ def summary(nickname: str):
 
     summary = summary_chain.invoke(use)
     print(summary)
+
+    recommand_parks = [use['park_1'], use['park_2'], use['park_3']]
+    recommand_cates = [use['category_1'], use['category_2'], use['category_3']]
+
     with conn.cursor() as cur:
         sql = """
         INSERT INTO tb_users_summary
@@ -85,10 +82,11 @@ def summary(nickname: str):
             use['create_date'],
             json.dumps(summary['top_emotions'], ensure_ascii=False),
             summary['emotions_summary'],
-            json.dumps(summary['recommand_cates'], ensure_ascii=False),
-            json.dumps(summary['recommand_parks'], ensure_ascii=False)
+            json.dumps(recommand_cates, ensure_ascii=False),
+            json.dumps(recommand_parks, ensure_ascii=False)
         ))
 
     conn.commit()
     conn.close()
     print('요약 끝!')
+    return summary
